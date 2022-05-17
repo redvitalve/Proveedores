042f364c254ea5f6fbbdf31faac9f97f0ab6c20d
Antonio Parejo <antonioparejo@gmail.com>
2022-05-17 17:06:19 -0400

    Agregando movimientos part1

--- app/Http/Livewire/EditProduct.php
+++ app/Http/Livewire/EditProduct.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\producto;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use App\Models\Movimiento;
 use Illuminate\Support\Facades\Storage;
 
 class EditProduct extends Component
@@ -12,9 +13,11 @@ class EditProduct extends Component
 
     use WithFileUploads;
     public $open = false;
-    public $producto,$sku_provee, $barras, $nombre, $cantidad_empaque, $cantidad, $condicion, $moneda, $cbulto, $cunidad, $psugerido, $user_id, $imagen, $identificador,$categoria;
+    public $imagentt,$producto,$sku_provee, $barras, $nombre, $cantidad_empaque, $cantidad, $condicion, $moneda, $cbulto, $cunidad, $psugerido, $user_id, $imagen, $identificador,$categoria;
     
 
+   
+    
     protected $rules = [
         'producto.nombre' => 'required',
         'producto.barras' => 'required',
@@ -38,13 +41,27 @@ class EditProduct extends Component
     {
         $this->producto->save();
 
+        $Msku_provee = 'producto.sku_provee';
+
+        Movimiento::create([
+            'sku_provee' => $this->$Msku_provee,
+            'condicion' => $this->condicion,
+            'moneda' => $this->moneda,
+            'cantidad' => $this->cantidad,
+            'cbulto' => $this->cbulto,
+            'cunidad' => $this->cunidad,
+            'psugerido' => $this->psugerido
+        ]);
+
         $this->reset(['open', 'barras', 'nombre', 'imagen', 'cantidad', 'categoria', 'cantidad_empaque', 'condicion', 
         'cbulto', 'cunidad', 'psugerido', 'moneda', 'sku_provee']);
 
         $this->identificador = rand();
         $this->emitTo('show-productos', 'render');
         $this->emit('alert', 'Se modifico satisfactoriamente');
+
     }
+
     public function render()
     {
         return view('livewire.edit-product');
